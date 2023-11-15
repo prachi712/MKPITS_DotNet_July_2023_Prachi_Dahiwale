156b7eb8d8adc0c9ae1ab51b9ea47701e1700154
prachi72 <prachidahiwale9922@gmail.com>
2023-11-15 08:48:43 -0800

    .

---
+++ C#/class_with_object.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7
+{
+    class Person
+    {
+        public  string name;
+         public int age;
+         public string address;
+        public void getdata(string name,int age,string address)
+        {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+    }
+}
