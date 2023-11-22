740346f11b007604b64003fb82c56015c66ba1a9
prachi72 <prachidahiwale9922@gmail.com>
2023-11-22 04:29:49 -0800

    .

---
+++ C#/abstract_class.cs.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_account
+{
+    abstract class abstract_class_account
+    {
+        public int actno;
+        public int bal = 1000;
+        public abstract string deposit(int a, int b);
+        public abstract string withdraw(int a, int b);
+        public string showbalance()
+        {
+            return "bal is=" + bal;
+
+        }
+    }
+    class saving : abstract_class_account
+    {
+        public override string deposit(int a, int b)
+        {
+            int interest = 500;
+            bal = bal + b + interest;
+            actno = a;
+            return "result:" + bal;
+        }
+        public override string withdraw(int a, int b)
+        {
+            int interest = 500;
+            bal = bal - b - interest;
+            actno = a;
+            return "result:" + bal;
+        }
+    }
+    class current : abstract_class_account
+    {
+        public override string deposit(int a, int b)
+        {
+            bal = bal + b;
+            return "bal is" + bal;
+        }
+        public override string withdraw(int a, int b)
+        {
+            bal = bal - b;
+            return "bal is" + bal;
+
+        }
+
+    }
+}
---
+++ C#/account.cs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace abstract_account
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            abstract_class_account acount = null;
+            int actno = Convert.ToInt32(textBox1.Text);
+            int amt = Convert.ToInt32(textBox1.Text);
+            string tt = textBox3.Text;
+            if (tt == "saving")
+            {
+                acount = new saving();
+            }
+            else if(tt=="current")
+            {
+                acount = new current(); 
+            }
+            label4.Text = acount.deposit(actno, amt);
+            label5.Text = acount.showbalance();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            abstract_class_account acount = null;
+            int actno = Convert.ToInt32(textBox1.Text);
+            int amt = Convert.ToInt32(textBox1.Text);
+            string tt = textBox3.Text;
+            if (tt == "saving")
+            {
+                acount = new saving();
+            }
+            else if (tt == "current")
+            {
+                acount = new current();
+            }
+            label4.Text = acount.withdraw(actno, amt);
+            label5.Text = acount.showbalance();
+        }
+    }
+}
