65b10b52955a938c98f673619a3ce54ba579016d
prachi72 <prachidahiwale9922@gmail.com>
2023-11-26 08:34:20 -0800

    .

---
+++ C#/abstract_class1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    abstract class shape
+    {
+        public abstract void draw();
+    }
+    class rectangle : shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("code to draw rectangle");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            shape s = new rectangle();
+            s.draw();
+            Console.ReadLine();
+        }
+    }
+}
