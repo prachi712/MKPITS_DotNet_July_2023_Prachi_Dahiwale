26115b8f950ddf53bd2d28554615fe2771d5ac86
prachi72 <prachidahiwale9922@gmail.com>
2023-12-10 11:23:44 -0500

    .

---
+++ C#/parameterized_constructor.cs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace program
+{
+    class Book
+    {
+        int bookid;
+        string title;
+        string author;
+        int price;
+        public Book()
+        {
+            Console.WriteLine("constructor with no parameter");
+            bookid = 123;
+            title = "mkpits";
+            author = "prachiii";
+            price = 2222;
+        }
+        public Book(int bookid,string title,string author,int price)
+        {
+            Console.WriteLine("constructor with no parameter");
+            this.bookid = bookid;
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+        public void display()
+        {
+            Console.WriteLine("bookid:" + bookid);
+            Console.WriteLine("title:" + title);
+            Console.WriteLine("author" + author);
+            Console.WriteLine("price:" + price);
+        }
+    }
+    class Program
+   
+        static void Main(string[]args)
+        {
+            Book b = new Book();
+            b.display();
+            Book b1 = new Book(22, "mkpits", "prachii", 70000);
+            b.display();
+        }
+    }
+}
---
+++ C#/static_member_and_static_method.cs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Program
+{
+    class book
+    {
+        static string title;
+        static book()
+        {
+            Console.WriteLine("static constructor called");
+            title = "mkpits";
+        }
+          public static void display()
+        {
+            Console.WriteLine("title:" + title);
+        }
+    }
+    class program
+    {
+        static void Main(string[]args)
+        {
+            book.display();
+            Console.ReadKey(); 
+        }
+    }
+}
