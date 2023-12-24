945595f1532c2f50c932a0d4435e289515c485a7
prachi72 <prachidahiwale9922@gmail.com>
2023-12-24 07:35:51 -0500

    .

---
+++ SQL main/practise4.sql
@@ -0,0 +1,9 @@
+select * from employees
+select * from employees
+where department-id=123
+select * from employees
+where department_name='prachi'
+select * from employees
+where department-id=456
+select * from employees
+where department_location='nagpur'
