521899deefb6805d1a44d297d49bc5e2a40a1729
prachi72 <prachidahiwale9922@gmail.com>
2024-01-16 07:17:16 -0500

    .

---
+++ Asp Dotnet/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace operator_if_else_if
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(TextBox1.Text);
+            int num2 = Convert.ToInt32(TextBox2.Text);
+            char op = Convert.ToChar(TextBox3.Text);
+            int res = 0;
+            if (op == '+')
+                res = num1 + num2;
+            else if (op == '-')
+                res = num1 - num2;
+            else if (op == '*')
+                res = num1 * num2;
+            else if (op == '/')
+                res = num1 / num2;
+            else
+                Label1.Text = "invalid operator";
+            Label1.Text = res.ToString();
+        }
+    }
+}
---
+++ Asp Dotnet/addition_of_two_numbers.aspx.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Addition : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(TextBox1.Text);
+            int num1 = Convert.ToInt32(TextBox2.Text);
+            int res = num + num1;
+            Label1.Text = res.ToString();
+        }
+    }
+}
